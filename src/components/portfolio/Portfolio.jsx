05ab4b14d5a90d6c41012140d4344bab42cb6271
Onome Oviero <onomevmeruwoma@yahoo.com>
2022-07-11 18:20:23 +0100

    second commit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,158 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.PNG'
+import IMG2 from '../../assets/portfolio2b.PNG'
+import IMG3 from '../../assets/portfolio3.PNG'
+import IMG4 from '../../assets/portfolio4b.PNG'
+import IMG5 from '../../assets/portfolio5.PNG'
+import IMG6 from '../../assets/portfolio5b.PNG'
+
+
+// Array method//
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Crypto Currency...',
+    github: 'https://github.com',
+    demo: 'https://dribble'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Crypto Currency...',
+    github: 'https://github.com',
+    demo: 'https://dribble'
+  },
+
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Crypto Currency...',
+    github: 'https://github.com',
+    demo: 'https://dribble'
+  },
+
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Crypto Currency...',
+    github: 'https://github.com',
+    demo: 'https://dribble'
+  },
+
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Crypto Currency...',
+    github: 'https://github.com',
+    demo: 'https://dribble'
+  },
+
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Crypto Currency...',
+    github: 'https://github.com',
+    demo: 'https://dribble'
+  },
+
+  
+]
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>My recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {/* <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com" className='btn' target='_blank'>Github</a>
+          <a href="https://dribble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG2} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com" className='btn' target='_blank'>Github</a>
+          <a href="https://dribble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG3} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com" className='btn' target='_blank'>Github</a>
+          <a href="https://dribble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG4} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com" className='btn' target='_blank'>Github</a>
+          <a href="https://dribble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG5} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com" className='btn' target='_blank'>Github</a>
+          <a href="https://dribble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG6} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com" className='btn' target='_blank'>Github</a>
+          <a href="https://dribble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live demo</a>
+          </div>
+        </article> */}
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return (
+              <article className='portfolio__item'>
+             <div className="portfolio__item-image">
+             <img src={image} alt={title} />
+          </div>
+          <h3>{title}</h3>
+          <div className="portfolio__item-cta">
+          <a href={github} className='btn' target='_blank'>Github</a>
+          <a href={demo} className='btn btn-primary' target='_blank'>Live demo</a>
+          </div>
+        </article>
+
+
+            )
+          })
+        }
+
+      </div>
+    </section>
   )
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,108 @@
 import React from 'react'
 import './testimonials.css'
+import AVAR1 from '../../assets/Tunde.PNG'
+import AVAR2 from '../../assets/Racheal.PNG'
+import AVAR3 from '../../assets/Funmi.PNG'
+
+
+// import Swiper core and required modules//
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles//
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+//array method//
+const data = [
+  {
+    avatar: AVAR1,
+    name: 'Tunde',
+    review: "Onome is that Front-end developer that thinks creatively and technically when programming. She is also a team player which is why I love working with her"
+  },
+
+  {
+    avatar: AVAR2,
+    name: 'Racheal',
+    review: "Onome's proactiveness is really commendable. She approaches her goals and tasks with determination and resilience. I admire her eagerness to learn and improve herself and her skills. She is definitely a gem and would benefit employers and other employees."
+  },
+
+  {
+    avatar: AVAR3,
+    name: 'Funmi',
+    review: "Onome makes a lasting impression of energy and passion through a warm and receptive personality. She is a good team player and she is always willing to help others. She tackles all assignments with dedication and enthusiasm. She is very focused and has aptitude to learn new things. She is an asset to any employer and I endorse her for any endeavor she chooses to pursue."
+  },
+]
+
+
 
 const Testimonials = () => {
   return (
-    <section id="testimonials">Testimonials</section>
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+
+      <Swiper className="container testimonials__container"
+      
+      // install Swiper modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+      >
+        {
+          data.map(({avatar, name, review}, index) => {
+            return(
+             <SwiperSlide key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt='Avatar' />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">
+               {review}
+              </small>
+             </SwiperSlide>
+
+            )
+          })     }
+      {/* <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVAR1} alt='Avatart 1' />
+          </div>
+          <h5 className="client__name">Tunde</h5>
+          <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum consequatur temporibus repellat eum, ducimus neque iure tempore dolore eius error!
+          </small>
+        </article>
+
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVAR1} alt='Avatart 1' />
+          </div>
+          <h5 className="client__name">Tunde</h5>
+          <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum consequatur temporibus repellat eum, ducimus neque iure tempore dolore eius error!
+          </small>
+        </article>
+
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVAR1} alt='Avatart 1' />
+          </div>
+          <h5 className="client__name">Tunde</h5>
+          <small className="client__review">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum consequatur temporibus repellat eum, ducimus neque iure tempore dolore eius error!
+          </small>
+        </article> */}
+
+      </Swiper>
+
+    </section>
   )
 }
 
